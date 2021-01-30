2626627b72410fd1ffcb0b9acfaab72a92738bc6
Rin Kaenbyou <ph3rin@gmail.com>
2021-01-29 22:12:57 -0800

    feature: 22:12

---
+++ Assets/Scripts/KeyPrefabs.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPrefabs : ScriptableObject
+{
+    [SerializeField] private List<Key> mPrefabs;
+
+    public Key GetKey(int index)
+    {
+        return mPrefabs[index];
+    }
+}

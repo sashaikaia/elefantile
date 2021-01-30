f9fe443ddbd6887f54ca77894368fe87eab7970f
Rin Kaenbyou <ph3rin@gmail.com>
2021-01-30 08:34:19 -0800

    feature: implemented simple follow camera

---
+++ Assets/Scripts/CameraFollowObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// This scripts allows the camera to follow the x-value of
+/// an object's transform.
+/// It respects the initial offset.
+/// </summary>
+public class CameraFollowObject : MonoBehaviour
+{
+    [SerializeField] private Transform mTargetTransform;
+    private float mOffset;
+    private void Start()
+    {
+        mOffset = transform.position.x - mTargetTransform.position.x;
+    }
+
+    private void Update()
+    {
+        var position = transform.position;
+        position.x = mTargetTransform.position.x + mOffset;
+        transform.position = position;
+    }
+}

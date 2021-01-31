cf6f8b80a67d194c65efa32df406327e4b5c2061
slyq <32990754+slyq@users.noreply.github.com>
2021-01-30 16:45:14 -0800

    Finish note submitter and note player

--- Assets/Scripts/Elephantile/NotePlayer.cs
+++ Assets/Scripts/Elephantile/NotePlayer.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// initializes and maps char to note sound clips
+// assign order A-G
 public class NotePlayer : MonoBehaviour
 {
     [SerializeField] private List<FMODUnity.StudioEventEmitter> mNoteList;
     private Dictionary<char, FMODUnity.StudioEventEmitter> mNoteMapper;
     // private const int mMaxNumNotes = 7;
-    void Awake() {
+    public void Awake() {
         mNoteMapper = new Dictionary<char, FMODUnity.StudioEventEmitter>();
         for (int i = 0; i < mNoteList.Count; ++i) {
             mNoteMapper.Add((char)('A'+i), mNoteList[i]);
         }
+    }
+
+    public void PlayNote(char n) {
+        mNoteMapper[n].GetComponent<FMODUnity.StudioEventEmitter>().Play();
     }
 }
--- Assets/Scripts/Elephantile/NoteSubmitter.cs
+++ Assets/Scripts/Elephantile/NoteSubmitter.cs
@@ -2,18 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoteSubmitter : MonoBehaviour
-{
-    private char mNextChar;
-    public void SubmitNote(char n) {
-        if (mNextChar == n) {
-
-        } else {
-            onError();
+namespace Elephantile {
+    public class NoteSubmitter : MonoBehaviour
+    {
+        private char mNextChar;
+        // "read only" definition of the correct notes
+        private LevelDefinition mLevelDef;
+        private int mLevelDefIndex;
+        [SerializeField] private LevelBase mLevelBase;
+        [SerializeField] private NotePlayer mNotePlayer;
+        public void Awake() {
+            mLevelDef = mLevelBase.GetDefinition();
+            mLevelDefIndex = 0;
         }
-    }
+        public void SubmitNote(char n) {
+            if (mLevelDef[mLevelDefIndex].pitch == n) {
+                mNotePlayer.PlayNote(n);
+            } else {
+                onError();
+            }
+        }
 
-    private void onError() {
-        Debug.Log("wrong note");
+        private void onError() {
+            Debug.Log("wrong note");
+        }
     }
 }

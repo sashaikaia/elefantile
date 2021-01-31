034a3e7ac8f67731c311543fb4779f1465632f3b
Tai <tainguye@usc.edu>
2021-01-30 17:55:56 -0800

    Menuloader
    feature: made a menu that transitions to the next level

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //The scene will transition to the next level
+        if (Input.GetMouseButtonDown(0))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex < 2)
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex+1));
+
+    }
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //play scene animation
+        transition.SetTrigger("Start");
+
+        //wait for animation to finish
+        yield return new WaitForSeconds(transitionTime);
+
+        //loads the scene
+        SceneManager.LoadScene(levelIndex);
+
+    }
+}
